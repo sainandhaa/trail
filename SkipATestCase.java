c751c0e561fa43a0978a23a064d25567ac6925ce
sainandhaa <101111049+sainandhaa@users.noreply.github.com>
2022-03-07 17:55:52 +0530

    intial commite

---
+++ SkipATestCase.java
@@ -0,0 +1,32 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class SkipATestCase {
+
+	
+	@Test(priority = 0)
+	public void startacar() {
+		System.out.println("Start the Car");
+	}
+	@Test(priority=5,enabled = false)
+	public void playmusic() {
+		System.out.println("play the music");
+	}
+	@Test(priority = 1)
+	public void putfirstgear() {
+		System.out.println("shift to the first gear");
+	}
+	@Test(priority = 2)
+	public void putsecondgear() {
+		System.out.println("shift to the second gear");
+	}
+	@Test(priority = 3)
+	public void putthirdgear() {
+		System.out.println("shift to the third gear");
+	}
+	@Test(priority = 4)
+	public void putforuthgear() {
+		System.out.println("shift to the foruth gear");
+	}
+}
